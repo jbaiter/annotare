c9c472069880c26f94ba36cdccc8ac7ee0786258
Johannes Baiter <johannes.baiter@gmail.com>
2016-02-29 01:49:14 +0100

    Fix JavaScript loading before body in production

--- src/clj/annotare/handler.clj
+++ src/clj/annotare/handler.clj
@@ -42,9 +42,8 @@
              :content  "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"}]
      (include-css "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css")
      (include-css "/css/screen.css")
-     [:body
-      mount-target
-      (include-js "/js/app.js")]]))
+     [:body mount-target]
+     (include-js "/js/app.js")]))
 
 (defroutes app-routes
   (GET "*" [] loading-page))
